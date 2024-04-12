cb9cf28e3337c4074243bd60af20796bd7a27c38
Cédric <cedtalbot@yahoo.fr>
2024-04-12 14:16:49 +0200

    Add infinite scroll to the autocomplete component

--- frontend/src/hooks/useDebounce.ts
+++ frontend/src/hooks/useDebounce.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 
-export const useDebounce = <T>(value: T, delay: number) => {
+export const useDebounce = <T>(value: T, delay: number, callback?: () => void) => {
   const [debouncedValue, setDebouncedValue] = useState<T>();
 
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedValue(value);
+      if (callback) callback();
     }, delay);
 
     return () => {
